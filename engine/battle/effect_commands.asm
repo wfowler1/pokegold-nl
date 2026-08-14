e1047d1ae0c3711446ec8be13a84e8c421d61187
William Fowler <will.fowler4201@gmail.com>
2026-08-14 00:08:07 -0600

    Make Sandstorm raise the Special Defense of Rock type Pokémon.

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1883,6 +1883,37 @@ BattleCommand_CheckPowder:
 	ld a, 1
 	ld [wAttackMissed], a
 	ret
+	
+SandstormSpDefBoost: 
+; First, check if Sandstorm is active.
+	ld a, [wBattleWeather]
+	cp WEATHER_SANDSTORM
+	ret nz
+
+; Then, check the opponent's types.
+; TODO: Use CheckIfTargetIsGivenType
+	ld hl, wEnemyMonType1
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .ok
+	ld hl, wBattleMonType1
+.ok
+	ld a, [hli]
+	cp ROCK
+	jr z, .start_boost
+	ld a, [hl]
+	cp ROCK
+	ret nz
+
+.start_boost
+	ld h, b
+	ld l, c
+	srl b
+	rr c
+	add hl, bc
+	ld b, h
+	ld c, l
+	ret
 
 BattleCommand_EffectChance:
 	xor a
@@ -2621,6 +2652,7 @@ PlayerAttackDamage:
 	ld b, a
 	ld c, [hl]
 
+	call SandstormSpDefBoost
 	ld a, [wEnemyScreens]
 	bit SCREENS_LIGHT_SCREEN, a
 	jr z, .specialcrit
@@ -2873,6 +2905,7 @@ EnemyAttackDamage:
 	ld b, a
 	ld c, [hl]
 
+	call SandstormSpDefBoost
 	ld a, [wPlayerScreens]
 	bit SCREENS_LIGHT_SCREEN, a
 	jr z, .specialcrit
