96b8d67585a9de4556a9edb28f63be5092479c45
William Fowler <will.fowler4201@gmail.com>
2026-08-16 18:46:27 -0600

    Update move description for Fairy Wind.

--- data/moves/descriptions.asm
+++ data/moves/descriptions.asm
@@ -1281,4 +1281,4 @@ DisarmingVoiceDescription:
 
 FairyWindDescription:
 	db   "Aanval doelwit met" ; "Hits target with a"
-	line "een fee windstroom@" ; "a fairy wind.@"
+	line "een feeënwind.@" ; "a fairy wind.@"
