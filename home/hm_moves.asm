e2ff2c0333db6839f3e67afa2bb5a6b1fe68bc74
William Fowler <will.fowler4201@gmail.com>
2026-08-18 21:24:25 -0600

    Allow forgetting HMs since they can be used without teaching them to your team now

--- home/hm_moves.asm
+++ home/hm_moves.asm
@@ -9,17 +9,17 @@ IsHM::
 	and a
 	ret
 
-IsHMMove::
-	ld hl, .HMMoves
-	ld de, 1
-	jp IsInArray
+;IsHMMove::
+;	ld hl, .HMMoves
+;	ld de, 1
+;	jp IsInArray
 
-.HMMoves:
-	db CUT
-	db FLY
-	db SURF
-	db STRENGTH
-	db FLASH
-	db WATERFALL
-	db WHIRLPOOL
-	db -1 ; end
+;.HMMoves:
+;	db CUT
+;	db FLY
+;	db SURF
+;	db STRENGTH
+;	db FLASH
+;	db WATERFALL
+;	db WHIRLPOOL
+;	db -1 ; end
