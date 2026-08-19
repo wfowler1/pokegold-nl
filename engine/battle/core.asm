cfce92c2ff05f1eb0dd3fc68aab0f15c75037311
William Fowler <will.fowler4201@gmail.com>
2026-08-18 21:16:13 -0600

    Add shiny indicator for in-battle UI

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4482,6 +4482,13 @@ PrintPlayerHUD:
 	ld hl, wPartyMon1DVs
 	call GetPartyLocation
 	ld de, wTempMonDVs
+	push hl
+	call BattleCheckPlayerShininess
+	jr nc, .not_shiny
+	hlcoord 18, 8
+	ld [hl], '♣'
+.not_shiny
+	pop hl
 	ld a, [hli]
 	ld [de], a
 	inc de
@@ -4591,6 +4598,14 @@ DrawEnemyHUD:
 .got_gender
 	hlcoord 9, 1
 	ld [hl], a
+	
+	push hl
+	call BattleCheckEnemyShininess
+	jr nc, .not_shiny
+	hlcoord 10, 1
+	ld [hl], '♣'
+.not_shiny
+	pop hl
 
 	hlcoord 2, 1
 	push af
