f145a54b18842bd1349c96bc1172d7093de82ddc
William Fowler <will.fowler4201@gmail.com>
2026-08-31 19:28:06 -0600

    Fix running sprite while crossing map boundaries.

--- engine/overworld/events.asm
+++ engine/overworld/events.asm
@@ -272,6 +272,11 @@ PlayerEvents:
 
 	ld [wScriptRunning], a
 	call DoPlayerEvent
+	ld a, [wScriptRunning]
+	cp PLAYEREVENT_CONNECTION
+	jr z, .ok2
+	cp PLAYEREVENT_JOYCHANGEFACING
+	jr z, .ok2
 
 ; Have player stand (resets running sprite to standing if event starts while running)
 	ld a, [wPlayerState]
